6231449f9932b70d350842d8ba4f1ee8dd8fe895
Don Owens <don@regexguy.com>
2025-12-31 15:07:11 -0800

    cleanup and made additions of data thread-safe

--- percentiles.go
+++ percentiles.go
@@ -3,10 +3,13 @@ package percentiles
 import (
 	// Built-in/core modules.
 	"bufio"
+	"fmt"
 	"iter"
 	"os"
+	"os/exec"
 	"slices"
 	"strconv"
+	"sync"
 )
 
 // Structure for object tracking percentile statistics.
@@ -24,6 +27,9 @@ type Stats struct {
 	// Length of the data added.
 	dataLen int64
 
+	// Mutex used to make data additions thread-safe.
+	dataLock *sync.Mutex
+
 	// Data length threshold at which we write to a file instead of keeping data
 	// in RAM.
 	fileThreshold int64
@@ -31,23 +37,78 @@ type Stats struct {
 
 // Creates and returns a new Stats instance.
 func NewStats() *Stats {
-	return &Stats{}
+	return &Stats{fileThreshold: 100000, dataLock: &sync.Mutex{}}
 }
 
-// Add a value to be tracked.
+// Set the data length threshold at which added data will be stored in a file,
+// rather than in RAM. The default is 100,000 data points.
+func (s *Stats) SetFileThreshold(threshold int64) {
+	s.fileThreshold = threshold
+}
+
+// Add a value to be tracked. Add() is thread-safe, but for better performance
+// when adding data from multiple threads, it is recommended to write to a
+// channel that is then processed by another goroutine that calls Add().
 func (s *Stats) Add(value float64) {
-	s.data = append(s.data, value)
+	s.dataLock.Lock()
+	defer s.dataLock.Unlock()
+
 	s.dataLen++
+
+	if s.dataFh != nil || s.dataLen > s.fileThreshold {
+		if s.dataFh == nil {
+			s.setupDataFile()
+		}
+		fmt.Fprintf(s.dataFh, "%f\n", value)
+		return
+	}
+
+	s.data = append(s.data, value)
+
 }
 
-// Add a slice of values to be tracked.
+// Add a slice of values to be tracked. AddSlice() is thread-safe, but for
+// better performance when adding data from multiple threads, it is recommended
+// to write to a channel that is then processed by another goroutine that calls
+// AddSlice().
 func (s *Stats) AddSlice(values []float64) {
-	s.data = append(s.data, values...)
+	s.dataLock.Lock()
+	defer s.dataLock.Unlock()
+
 	s.dataLen += int64(len(values))
+
+	if s.dataFh != nil || s.dataLen > s.fileThreshold {
+		if s.dataFh == nil {
+			s.setupDataFile()
+		}
+		for _, value := range values {
+			fmt.Fprintf(s.dataFh, "%f\n", value)
+		}
+		return
+	}
+
+	s.data = append(s.data, values...)
 }
 
-// Returns commonly-used percentiles as a slice of float64: p50, p75, p90, p95,
-// and p99.
+func (s *Stats) setupDataFile() error {
+	if s.dataFh != nil {
+		// Already set up.
+		return nil
+	}
+
+	var err error
+
+	s.dataFh, err = os.CreateTemp("", "pct_data_")
+	for _, value := range s.data {
+		fmt.Fprintf(s.dataFh, "%f\n", value)
+	}
+	s.data = nil
+
+	return err
+}
+
+// Returns commonly-used percentiles as a slice of float64: p50 (median), p75,
+// p90, p95, and p99.
 func (s *Stats) Percentiles() []float64 {
 	s.sortData()
 
@@ -114,12 +175,13 @@ func (s *Stats) Percentile(p float64) float64 {
 	return lastVal
 }
 
-// Clean up resources.
+// Free up resources.
 func (s *Stats) Done() {
 	if s.dataFh != nil {
 		s.dataFh.Close()
 		os.Remove(s.dataFh.Name())
 	}
+	s.data = nil
 }
 
 func (s *Stats) iterateData() iter.Seq[float64] {
@@ -145,13 +207,52 @@ func (s *Stats) iterateData() iter.Seq[float64] {
 	}
 }
 
-func (s *Stats) sortData() {
+// Sorts the input data. If the data is in a file, the external sort command is
+// called.
+func (s *Stats) sortData() error {
+	s.dataLock.Lock()
+	defer s.dataLock.Unlock()
+
+	if s.dataSorted {
+		return nil
+	}
+
 	if s.dataFh != nil {
-		// TODO: Implement sorting from file.
+		s.dataFh.Close()
+		oldFileName := s.dataFh.Name()
+		defer os.Remove(oldFileName)
+
+		newFh, err := os.CreateTemp("", "pct_data_sort_")
+		if err != nil {
+			return err
+		}
+
+		if err = s.callSort(s.dataFh.Name(), newFh.Name()); err != nil {
+			return err
+		}
+
+		s.dataFh = newFh
+		s.dataSorted = true
+
+		return nil
 	}
 
 	if !s.dataSorted {
 		slices.Sort(s.data)
 		s.dataSorted = true
 	}
+
+	return nil
+}
+
+// Calls the external sort command to sort floating point numbers in infile and
+// write the results to outfile.
+func (s *Stats) callSort(infile, outfile string) error {
+	sort_path := "/usr/bin/sort"
+	cmd := &exec.Cmd{
+		Path: sort_path,
+		Args: []string{sort_path, "--mergesort", "-n", "-o", outfile, infile},
+	}
+
+	return cmd.Run()
 }
--- percentiles_test.go
+++ percentiles_test.go
@@ -2,6 +2,7 @@ package percentiles_test
 
 import (
 	// Built-in/core modules
+	"fmt"
 	"testing"
 
 	// Third-party modules.
@@ -12,7 +13,19 @@ import (
 )
 
 func TestPercent(t *testing.T) {
+	t.Run("default_threshold", func(st *testing.T) {
+		runWithThreshold(st, 0)
+	})
+
+	t.Run("threshold=5", func(st *testing.T) {
+		runWithThreshold(st, 5)
+	})
+
+}
+
+func runWithThreshold(t *testing.T, threshold int64) {
 	p := percentiles.NewStats()
+	p.SetFileThreshold(threshold)
 	for i := range 100 {
 		// Add values in reverse order to ensure sorting works.
 		p.Add(float64(100 - i))
@@ -32,4 +45,25 @@ func TestPercent(t *testing.T) {
 	result100 := p.Percentile(100.0)
 
 	assert.Equal(t, 100.0, result100)
+}
+
+func ExampleStats_Percentiles() {
+	p := percentiles.NewStats()
+
+	for i := range 100 {
+		p.Add(float64(100 - i))
+	}
+
+	// Returns p50, p75, p90, p95, and p99.
+	results := p.Percentiles()
+	fmt.Printf("percentiles: %v\n", results)
+
+	// Returns a specific percentile:
+	result87 := p.Percentile(87.0)
+
+	fmt.Printf("87th percentile: %v\n", result87)
+
+	// Output:
+	// percentiles: [50 75 90 95 99]
+	// 87th percentile: 87
 }
